849c3fba12d79ff1280df9025c4ab290f857d274
Suarez <ivan.suarez@emergingtravel.com>
2026-08-16 19:14:41 +0300

    Arch fixes

--- pet_hotel/settings.py
+++ pet_hotel/settings.py
@@ -18,7 +18,7 @@ ENVIRONMENT_PROD = "PROD"
 ENVIRONMENT = os.environ.get('ENVIRONMENT', "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 0)
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
 if ENVIRONMENT == ENVIRONMENT_DEV:
     ALLOWED_HOSTS = ["*"]
